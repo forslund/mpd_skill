9efa4ce75a737ff1e0883f9bc42aad2b584bb91f
Åke Forslund <ake.forslund@gmail.com>
2022-11-27 12:38:34 +0100

    Remove search for title it doesn't seem to be valid

--- __init__.py
+++ __init__.py
@@ -122,12 +122,10 @@ class MPDSkill(CommonPlaySkill):
             self.artists = self.server.list('artist')
             self.log.info('Genres...')
             self.genres = self.server.list('genre')
-            self.log.info('Titles...')
-            self.titles = self.server.list('title')
+            self.log.info(self.genres)
             self.log.info('Done!')
 
-            self.playlist = (self.albums + self.artists +
-                             self.genres + self.titles)
+            self.playlist = self.albums + self.artists + self.genres
             self.register_vocabulary(self.name, 'NameKeyword')
             return True
         except Exception:
